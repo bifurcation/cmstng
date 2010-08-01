ac3f6815e585258ba8b5167060d13f3da858bfa5
rescorla <rescorla@60afc23b-3570-b480-5588-d332f296fb10>
2010-08-01 13:00:11 +0000

    Commit

---
+++ example.py
@@ -0,0 +1,50 @@
+import json
+import datetime
+import base64
+
+# VErsion
+version = "1.0"
+
+# The interior message we want to protect
+message_text = "This is a test message"
+
+n = datetime.datetime.utcnow()
+date = .strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+inner_message = {
+    'ContentType':"text/plain",
+    'Data': message_text
+    }
+
+
+def sign_message(msg, signer_cert, signer_priv, digest_algorithm="SHA1"):
+    msg['date'] = date
+    msg_b64 = base64.b64encode(json.dumps(msg))
+
+    username = signer_certs[0].getUsername()
+    signature = signer_priv.sign(msg_b64)
+
+    pkix_chain = []
+    for cert in signer_certs:
+        pkix_chain.append(cert.getBase64())
+            
+    smsg = {
+	"Version":version,
+        "Signature":{
+            "DigestAlgorithm":digest_algorithm,
+            "SignatureAlgorithm":signer_priv.getAlgorithm(),
+            "PkixChain":pkix_chain
+            }
+        "SignedData":msg_b64
+        }
+    
+
+
+    
+
+
+
+    
+
+
