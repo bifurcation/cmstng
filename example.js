4fc6615f66115b9c30f360836ef83b70af61002d
hildjj@gmail.com <hildjj@gmail.com@60afc23b-3570-b480-5588-d332f296fb10>
2010-08-01 11:56:04 +0000

    signature block

--- example.js
+++ example.js
@@ -1,10 +1,21 @@
-var msg = { 'Signer':'joe@webex.com', 
-			'Signature':'123457809009089989',
-			'Algorithm':'RSA',
-			'SignedData':b64({
-						   'ContentType': 'text/plain', 
-						   'Date': '2010-08-01T11:42:27Z',
-						   'Data': 'foo'
-					   })};
+var msg = { 
+	'Version':'1.0',
+	'Signature':{
+		'Value':b64(sig),
+		'DigestAlgorithm':'SHA-1',
+		'SignatureAlgorithm':'RSA-PKCS1-1.5',
+		'Signer':'hildjj@jabber.org', // XOR certs
+		'PKIXChain':[
+			b64(certMe),
+			b64(certSigner),
+			b64(certCA)
+			// ...
+		]
+	},
+	'SignedData':b64({
+						 'ContentType': 'text/plain', 
+						 'Date': '2010-08-01T11:42:27Z',
+						 'Data': 'foo'
+					 })};
 
 
