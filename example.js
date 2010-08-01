6320476643251ad4f3324a061c36781044c3a3c3
hildjj@gmail.com <hildjj@gmail.com@60afc23b-3570-b480-5588-d332f296fb10>
2010-08-01 11:25:05 +0000

    Added example

---
+++ example.js
@@ -0,0 +1,7 @@
+var msg = { 'signer':'joe@webex.com', 
+			'signature':'123457809009089989',
+			'body':b64({
+						   'Content-Type': 'text/plain', 
+						   'Date': 'today',
+						   'Data': 'foo'
+					   })};
