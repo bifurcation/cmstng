9853a1d39f9947b2d1e7a354e4a45315c121353c
hildjj@gmail.com <hildjj@gmail.com@60afc23b-3570-b480-5588-d332f296fb10>
2010-08-01 15:13:21 +0000

    Add first pass of encrypt/decrypt

--- crypto.py
+++ crypto.py
@@ -1,4 +1,8 @@
 import json
+from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+import keyczar.keys
+import keyczar.util
 
 class Certificate(object):
     def __init__(self, encoding):
@@ -20,39 +24,61 @@ class Certificate(object):
 class PublicKey(object):
     def __init__(self, encoding):
         self.encoding_ = encoding
+        parsed = json.loads(self.encoding_)
+        n = long(parsed['n'])
+        e = long(parsed['e'])
+        self.key = RSA.construct((n, e))
 
     def verify(self, signature, signature_algorithm, digest_algorithm, signed_data):
         return True
 
     def encrypt(self, plaintext):
-        return "Encrypted key"
+        return self.key.encrypt(plaintext, None)[0]
 
     def getBase64(self):
         return self.encoding_.encode("base64")
 
+    def __str__(self):
+        return self.encoding_
+
 class PrivateKey(object):
     def __init__(self, encoding):
         self.encoding_ = encoding
+        parsed = json.loads(self.encoding_)
+        n = long(parsed['n'])
+        e = long(parsed['e'])
+        d = long(parsed['d'])
+        p = long(parsed['p'])
+        q = long(parsed['q'])
+        self.key = RSA.construct((n, e, d, p, q))
 
     def sign(self, digest_algorithm, signed_data):
         return "Signature" + "+" + digest_algorithm
 
     def decrypt(self, ciphertext):
-        return "Decrypted"
+        return self.key.decrypt(ciphertext)
 
     def getAlgorithm(self):
         return "RSA-PKCS1-1.5"
     
-    
+    def __str__(self):
+        return self.encoding_
 
-    
+class KeyPair(object):
+    def __init__(self, size=1024):
+        self.priv = keyczar.keys.GenKey(keyczar.keyinfo.RSA_PRIV, size)
 
+    def getPubkey(self):
+        jpub = json.dumps(self.priv.public_key.key.key.__dict__)
+        return PublicKey(jpub)
 
+    def getPrivkey(self):
+        jpriv = json.dumps(self.priv.key.key.__dict__)
+        return PrivateKey(jpriv)
 
-
-    
-        
-
-
-
-
+if __name__ == '__main__':
+    pair = KeyPair(384)
+    priv = pair.getPrivkey()
+    pub = pair.getPubkey()
+    ciphertext = pub.encrypt("foo")
+    print priv.decrypt(ciphertext)
