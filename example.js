2771b6297eb4f4ba29d5bc331545f07ceb5fcb1e
hildjj@gmail.com <hildjj@gmail.com@60afc23b-3570-b480-5588-d332f296fb10>
2010-08-01 12:02:55 +0000

    Updates

--- example.js
+++ example.js
@@ -5,16 +5,19 @@ var msg = {
 		'DigestAlgorithm':'SHA-1',
 		'SignatureAlgorithm':'RSA-PKCS1-1.5',
 		'Signer':'hildjj@jabber.org', // XOR certs
-		'PKIXChain':[
+		'PkixChain':[ // RFC 5280
 			b64(certMe),
 			b64(certSigner),
 			b64(certCA)
 			// ...
+		],
+		'PkixCRL':[  // Leave it out?
 		]
 	},
 	'SignedData':b64({
-						 'ContentType': 'text/plain', 
-						 'Date': '2010-08-01T11:42:27Z',
+						 // TODO: signer name?
+						 'ContentType': 'text/plain', // MIME type
+						 'Date': '2010-08-01T11:42:27Z', // RFC 3339
 						 'Data': 'foo'
 					 })};
 
