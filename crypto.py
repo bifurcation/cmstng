3da238a63c399525a3f37deb3492bde4c338a3e3
rescorla <rescorla@60afc23b-3570-b480-5588-d332f296fb10>
2010-08-01 14:30:51 +0000

    Sign and verify

--- crypto.py
+++ crypto.py
@@ -1,20 +1,49 @@
+import json
+
 class Certificate(object):
-    def __init__(self, encoding, parsed=None):
-        self.encoding_ = encoding
-        if parsed != None:
-            self.username_ = parsed['username']
-            self.pubkey_ = parsed['pubkey']
+    def __init__(self, encoding):
+        self.encoding_ = encoding.decode("base64")
+        parsed=json.loads(self.encoding_)
+        self.username_ = parsed['username']
+        self.pubkey_ = parsed['pubkey']
 
     def getUsername(self):
         return self.username_
 
     def getBase64(self):
-        return self.encoding_
+        return self.encoding_.encode("base64")
 
     def getPubkey(self):
-        return self.pubkey_
+        return PublicKey(self.pubkey_)
 
 
+class PublicKey(object):
+    def __init__(self, encoding):
+        self.encoding_ = encoding
+
+    def verify(self, signature, signature_algorithm, digest_algorithm, signed_data):
+        return True
+
+    def encrypt(self, plaintext):
+        return "Encrypted key"
+
+    def getBase64(self):
+        return self.encoding_.encode("base64")
+
+class PrivateKey(object):
+    def __init__(self, encoding):
+        self.encoding_ = encoding
+
+    def sign(self, digest_algorithm, signed_data):
+        return "Signature" + "+" + digest_algorithm
+
+    def decrypt(self, ciphertext):
+        return "Decrypted"
+
+    def getAlgorithm(self):
+        return "RSA-PKCS1-1.5"
+    
+    
 
     
 
--- example.py
+++ example.py
@@ -1,29 +1,29 @@
 import json
 import datetime
 import base64
+import crypto
 
 # VErsion
 version = "1.0"
 
 # The interior message we want to protect
 message_text = "This is a test message"
-
-n = datetime.datetime.utcnow()
-date = .strftime("%Y-%m-%dT%H:%M:%SZ")
-
-
 inner_message = {
     'ContentType':"text/plain",
     'Data': message_text
     }
 
 
-def sign_message(msg, signer_cert, signer_priv, digest_algorithm="SHA1"):
-    msg['date'] = date
+def get_date():
+    n = datetime.datetime.utcnow()
+    return n.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def sign_message(msg, signer_certs, signer_priv, digest_algorithm="SHA1"):
+    msg['date'] = get_date()
     msg_b64 = base64.b64encode(json.dumps(msg))
 
     username = signer_certs[0].getUsername()
-    signature = signer_priv.sign(msg_b64)
+    signature = signer_priv.sign(digest_algorithm, msg_b64)
 
     pkix_chain = []
     for cert in signer_certs:
@@ -34,16 +34,63 @@ def sign_message(msg, signer_cert, signer_priv, digest_algorithm="SHA1"):
         "Signature":{
             "DigestAlgorithm":digest_algorithm,
             "SignatureAlgorithm":signer_priv.getAlgorithm(),
-            "PkixChain":pkix_chain
-            }
+            "PkixChain":pkix_chain,
+            "Signer":username,
+            "Value":signature
+            },
         "SignedData":msg_b64
         }
 
-    
-    
+    return smsg
+
+def verify_message(msg):
+    try:
+        signature = msg['Signature']
+        pkix_chain = []
+        
+        # TODO: Check version
+
+        for cert in signature['PkixChain']:
+            pkix_chain.append(crypto.Certificate(cert))
+        # TODO: Verify the cert chain
+
+
+        if pkix_chain[0].getUsername() != signature['Signer']:
+            raise Exception("Mismatched usernames: %s != %s"%(pkix_chain[0].getUsername(), signature['Signer']))
+
+        return pkix_chain[0].getPubkey().verify(signature['Value'],
+                                                signature['SignatureAlgorithm'],
+                                                signature['DigestAlgorithm'],
+                                                msg['SignedData'])
+
+    except KeyError, e:
+        raise Exception("Malformed message, missing key: %s",str(e))
 
 
     
+if __name__ == '__main__':
+    ekr_cert_json = {
+        'username':'ekr@rtfm.com',
+        'pubkey':crypto.PublicKey("EKRPub").getBase64()
+        }
+                    
+    ekr_certs = [ crypto.Certificate( json.dumps(ekr_cert_json).encode("base64")) ]
+    ekr_priv = crypto.PrivateKey("EKRPriv")
+
+    # Sign a message
+    signed = sign_message(inner_message, ekr_certs, ekr_priv)
+    print "***** Signed message ****"
+    print json.dumps(signed)
+    print "*****"
+
+
+    # Verify a message
+    result = verify_message(signed)
+    if result != True:
+        print "Verification failed"
+    else:
+        print "Verification succceeded"
+        
 
 
 
