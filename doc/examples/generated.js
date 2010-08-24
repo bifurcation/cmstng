15fa2416547b11ea6013e3750489ae8fee62ac63
hildjj@gmail.com <hildjj@gmail.com@60afc23b-3570-b480-5588-d332f296fb10>
2010-08-24 07:02:18 +0000

    New examples

--- doc/examples/generated.js
+++ doc/examples/generated.js
@@ -1,57 +1,121 @@
+var ca_priv={
+  "PublicKey": {
+    "RsaExponent": "AQAB", 
+    "Type": "publickey", 
+    "Algorithm": "RSA-PKCS1-1.5", 
+    "RsaModulus": "6xSBAmJuH49H/LAuViYvwh2TihNvjBJQsX8IVbPVDN8X4Nu9ZopMdBvrw0veB/ad5pQTRwRt65oIlETH8+K/BQ=="
+  }, 
+  "Version": "1.0", 
+  "Type": "privatekey", 
+  "Algorithm": "RSA-PKCS1-1.5", 
+  "PrivateExponent": "JlWGoBvq90yHIaadN8EsYowuJgL7Wq0V6YvckRUtNdbPa8UwVF15VFgEBOcu5lSdRDel9O9vejW08rdiSY56FQ=="
+};
+var ca_cert={
+  "Hash": "4XJ76Jbs4U5SPekBQO1xhOkZDts=", 
+  "Name": "My CA", 
+  "NotBefore": "2010-08-23T21:02:54Z", 
+  "NotAfter": "2011-08-23T21:02:54Z", 
+  "PublicKey": {
+    "RsaExponent": "AQAB", 
+    "Type": "publickey", 
+    "Algorithm": "RSA-PKCS1-1.5", 
+    "RsaModulus": "6xSBAmJuH49H/LAuViYvwh2TihNvjBJQsX8IVbPVDN8X4Nu9ZopMdBvrw0veB/ad5pQTRwRt65oIlETH8+K/BQ=="
+  }, 
+  "Version": "1.0", 
+  "Type": "certificate"
+};
 var priv={
   "PublicKey": {
     "RsaExponent": "AQAB", 
     "Type": "publickey", 
     "Algorithm": "RSA-PKCS1-1.5", 
-    "RsaModulus": "0KRfrv1lg84K1vsweRlW/biQlf3e9ZiccPMGmhDcUhtcH+0DGmd9ESN3xpcvZ56yjjVlpuXdJ+8h4Bmqnm3fZw=="
+    "RsaModulus": "ipucQMUITLxemi0bb3tNI/vwjGSMCErQENlf4c8A2HNEoKap05q6+6JJnE4zHGBFZRpUb6MgJAZR9mELmjEGLQ=="
   }, 
   "Version": "1.0", 
   "Type": "privatekey", 
   "Algorithm": "RSA-PKCS1-1.5", 
-  "PrivateExponent": "xu9zrE5ANOSIwWLQXSckIteQRPPDWzkgMTIH4W6iJEXpWRdA3SjBNNrOqLZhMutzNF/vV8d5ziA1XMOVEMAQgQ=="
+  "PrivateExponent": "W2TBwd9pWpe2VTi652ZI9howgZHm16zkxsdeNpl0uENEy0sNaE9jwvI9NcACfzoSYzomM9HSRjp/4yobJf3TQQ=="
 };
 var pub={
   "RsaExponent": "AQAB", 
   "Type": "publickey", 
   "Algorithm": "RSA-PKCS1-1.5", 
-  "RsaModulus": "0KRfrv1lg84K1vsweRlW/biQlf3e9ZiccPMGmhDcUhtcH+0DGmd9ESN3xpcvZ56yjjVlpuXdJ+8h4Bmqnm3fZw=="
+  "RsaModulus": "ipucQMUITLxemi0bb3tNI/vwjGSMCErQENlf4c8A2HNEoKap05q6+6JJnE4zHGBFZRpUb6MgJAZR9mELmjEGLQ=="
 };
-cert={
+var cert={
+  "Hash": "2bSMFkSvJg4YWXsiGso7CcCVGyw=", 
   "Name": "joe@example.com", 
-  "NotBefore": "2010-08-15T04:35:49Z", 
-  "NotAfter": "2010-08-22T04:35:49Z", 
+  "NotBefore": "2010-08-23T21:02:54Z", 
+  "NotAfter": "2011-08-23T21:02:54Z", 
   "PublicKey": {
     "RsaExponent": "AQAB", 
     "Type": "publickey", 
     "Algorithm": "RSA-PKCS1-1.5", 
-    "RsaModulus": "0KRfrv1lg84K1vsweRlW/biQlf3e9ZiccPMGmhDcUhtcH+0DGmd9ESN3xpcvZ56yjjVlpuXdJ+8h4Bmqnm3fZw=="
+    "RsaModulus": "ipucQMUITLxemi0bb3tNI/vwjGSMCErQENlf4c8A2HNEoKap05q6+6JJnE4zHGBFZRpUb6MgJAZR9mELmjEGLQ=="
   }, 
   "Version": "1.0", 
   "Type": "certificate"
-}
-var sig={
-  "SignedData": "eyJEYXRlIjogIjIwMTAtMDgtMTVUMDQ6MzU6NDlaIiwgIkRhdGEiOiAiRm9vIiwgIlZlcnNpb24iOiAiMS4wIiwgIlR5cGUiOiAiaW5uZXIiLCAiQ29udGVudFR5cGUiOiAidGV4dC9wbGFpbiJ9", 
+};
+var signed_cert={
+  "SignedData": "eyJEYXRlIjogIjIwMTAtMDgtMjNUMjE6MDI6NTRaIiwgIkRhdGEiOiAie1wiSGFzaFwiOiBcIjJiU01Ga1N2Smc0WVdYc2lHc283Q2NDVkd5dz1cIiwgXCJOYW1lXCI6IFwiam9lQGV4YW1wbGUuY29tXCIsIFwiTm90QmVmb3JlXCI6IFwiMjAxMC0wOC0yM1QyMTowMjo1NFpcIiwgXCJOb3RBZnRlclwiOiBcIjIwMTEtMDgtMjNUMjE6MDI6NTRaXCIsIFwiUHVibGljS2V5XCI6IHtcIlJzYUV4cG9uZW50XCI6IFwiQVFBQlwiLCBcIlR5cGVcIjogXCJwdWJsaWNrZXlcIiwgXCJBbGdvcml0aG1cIjogXCJSU0EtUEtDUzEtMS41XCIsIFwiUnNhTW9kdWx1c1wiOiBcImlwdWNRTVVJVEx4ZW1pMGJiM3ROSS92d2pHU01DRXJRRU5sZjRjOEEySE5Fb0thcDA1cTYrNkpKbkU0ekhHQkZaUnBVYjZNZ0pBWlI5bUVMbWpFR0xRPT1cIn0sIFwiVmVyc2lvblwiOiBcIjEuMFwiLCBcIlR5cGVcIjogXCJjZXJ0aWZpY2F0ZVwifSIsICJWZXJzaW9uIjogIjEuMCIsICJUeXBlIjogImlubmVyIiwgIkNvbnRlbnRUeXBlIjogImFwcGxpY2F0aW9uL2pzb24ifQ==", 
   "Version": "1.0", 
   "Type": "signed", 
   "Signature": {
+    "Certificate": {
+      "Hash": "4XJ76Jbs4U5SPekBQO1xhOkZDts=", 
+      "Name": "My CA", 
+      "NotBefore": "2010-08-23T21:02:54Z", 
+      "NotAfter": "2011-08-23T21:02:54Z", 
+      "PublicKey": {
+        "RsaExponent": "AQAB", 
+        "Type": "publickey", 
+        "Algorithm": "RSA-PKCS1-1.5", 
+        "RsaModulus": "6xSBAmJuH49H/LAuViYvwh2TihNvjBJQsX8IVbPVDN8X4Nu9ZopMdBvrw0veB/ad5pQTRwRt65oIlETH8+K/BQ=="
+      }, 
+      "Version": "1.0", 
+      "Type": "certificate"
+    }, 
     "DigestAlgorithm": "SHA1", 
-    "Value": "apsrYI/UZjDF6GaYUsBjtksAgUq1t0PQ4FEXt4vziUgYuDr0DcvSo3a2RcTlCYnMhZZneGkvXnCPEU5D50HjmA==", 
-    "Signer": "joe@example.com", 
-    "PkixChain": [
-      {
-        "Name": "joe@example.com", 
-        "NotBefore": "2010-08-15T04:35:49Z", 
-        "NotAfter": "2010-08-22T04:35:49Z", 
-        "PublicKey": {
-          "RsaExponent": "AQAB", 
-          "Type": "publickey", 
-          "Algorithm": "RSA-PKCS1-1.5", 
-          "RsaModulus": "0KRfrv1lg84K1vsweRlW/biQlf3e9ZiccPMGmhDcUhtcH+0DGmd9ESN3xpcvZ56yjjVlpuXdJ+8h4Bmqnm3fZw=="
+    "Value": "fRC7WiQREGQ25kY2nsTCOUSJ4etWaPA+fJ92UXxdu1Y1k0mUz/YEnG9MYv7M7hOEibAtXByaRTLie6QlSj5ssA==", 
+    "Signer": "My CA", 
+    "Type": "signature", 
+    "SignatureAlgorithm": "RSA-PKCS1-1.5"
+  }
+};
+var sig={
+  "SignedData": "eyJEYXRlIjogIjIwMTAtMDgtMjNUMjE6MDI6NTRaIiwgIkRhdGEiOiAiRm9vIiwgIlZlcnNpb24iOiAiMS4wIiwgIlR5cGUiOiAiaW5uZXIiLCAiQ29udGVudFR5cGUiOiAidGV4dC9wbGFpbiJ9", 
+  "Version": "1.0", 
+  "Type": "signed", 
+  "Signature": {
+    "Certificate": {
+      "SignedData": "eyJEYXRlIjogIjIwMTAtMDgtMjNUMjE6MDI6NTRaIiwgIkRhdGEiOiAie1wiSGFzaFwiOiBcIjJiU01Ga1N2Smc0WVdYc2lHc283Q2NDVkd5dz1cIiwgXCJOYW1lXCI6IFwiam9lQGV4YW1wbGUuY29tXCIsIFwiTm90QmVmb3JlXCI6IFwiMjAxMC0wOC0yM1QyMTowMjo1NFpcIiwgXCJOb3RBZnRlclwiOiBcIjIwMTEtMDgtMjNUMjE6MDI6NTRaXCIsIFwiUHVibGljS2V5XCI6IHtcIlJzYUV4cG9uZW50XCI6IFwiQVFBQlwiLCBcIlR5cGVcIjogXCJwdWJsaWNrZXlcIiwgXCJBbGdvcml0aG1cIjogXCJSU0EtUEtDUzEtMS41XCIsIFwiUnNhTW9kdWx1c1wiOiBcImlwdWNRTVVJVEx4ZW1pMGJiM3ROSS92d2pHU01DRXJRRU5sZjRjOEEySE5Fb0thcDA1cTYrNkpKbkU0ekhHQkZaUnBVYjZNZ0pBWlI5bUVMbWpFR0xRPT1cIn0sIFwiVmVyc2lvblwiOiBcIjEuMFwiLCBcIlR5cGVcIjogXCJjZXJ0aWZpY2F0ZVwifSIsICJWZXJzaW9uIjogIjEuMCIsICJUeXBlIjogImlubmVyIiwgIkNvbnRlbnRUeXBlIjogImFwcGxpY2F0aW9uL2pzb24ifQ==", 
+      "Version": "1.0", 
+      "Type": "signed", 
+      "Signature": {
+        "Certificate": {
+          "Hash": "4XJ76Jbs4U5SPekBQO1xhOkZDts=", 
+          "Name": "My CA", 
+          "NotBefore": "2010-08-23T21:02:54Z", 
+          "NotAfter": "2011-08-23T21:02:54Z", 
+          "PublicKey": {
+            "RsaExponent": "AQAB", 
+            "Type": "publickey", 
+            "Algorithm": "RSA-PKCS1-1.5", 
+            "RsaModulus": "6xSBAmJuH49H/LAuViYvwh2TihNvjBJQsX8IVbPVDN8X4Nu9ZopMdBvrw0veB/ad5pQTRwRt65oIlETH8+K/BQ=="
+          }, 
+          "Version": "1.0", 
+          "Type": "certificate"
         }, 
-        "Version": "1.0", 
-        "Type": "certificate"
+        "DigestAlgorithm": "SHA1", 
+        "Value": "fRC7WiQREGQ25kY2nsTCOUSJ4etWaPA+fJ92UXxdu1Y1k0mUz/YEnG9MYv7M7hOEibAtXByaRTLie6QlSj5ssA==", 
+        "Signer": "My CA", 
+        "Type": "signature", 
+        "SignatureAlgorithm": "RSA-PKCS1-1.5"
       }
-    ], 
+    }, 
+    "DigestAlgorithm": "SHA1", 
+    "Value": "cbbloWYzWuvJLy4HF+p7WxcJ9L9ZboZWQwJqJh30YH+jyZsRIQ9uh7T9xrjPp3AIZW/UgBa3IC52tg6ThRvJJQ==", 
+    "Signer": "joe@example.com", 
     "Type": "signature", 
     "SignatureAlgorithm": "RSA-PKCS1-1.5"
   }
@@ -61,22 +125,22 @@ var encrypted={
   "Recipients": [
     {
       "EncryptionAlgorithm": "RSA-PKCS1-1.5", 
+      "CertificateHash": "2bSMFkSvJg4YWXsiGso7CcCVGyw=", 
       "Type": "recipient", 
       "Name": "joe@example.com", 
-      "PkixCertificateHash": "TODO:HASH CERTS", 
-      "EncryptionKey": "uoGTSivP3mi0+qhzmfCHoqeTbIxeMpe64TL/6zNvYSS16Pkgq9K1OC1pmd/p5UBcwuKPv12sNXaAsig9ndCUdg=="
+      "EncryptionKey": "T0mSGyLsI+AIuzdFpGuOvKIEix6FklhtgbP+Fat400sfNkDM9amqnkJGhQWxYHSpp34+XnAZKOPHj20EYeDuTQ=="
     }
   ], 
   "Encryption": {
     "Type": "encryption", 
     "Algorithm": "AES-256-CBC", 
-    "IV": "VJuFK1sRrrbeHwCvsxxGGA=="
+    "IV": "c1c9L6HmjGp7LuHyxnQKxg=="
   }, 
   "Version": "1.0", 
-  "EncryptedData": "yzCjVdlSZiaI+QKNzg1GA396Ly8vZG+xAChlXhLlLeeqtF/uqVIcF791DgPOdU0/PKDF1T1jnDeUZHMlUlkpl1JHQhQvoD+s4DtWMixlr/20xF4CeTlhieddqQoGjtRsn6siqKczR3p2tNHy0IoxsQ==", 
+  "EncryptedData": "YCnrKkEexotgWJ4K/gvsKACL3rn4DmVKFhUZahoTIyLN53WyO1UCEAvn7QEMX/fCsRQjlb1+OqP9AevBGyBHLShgm5LxFXjlJh43hHds5EKCApAH22cyqVZAKYBYeUlV6JqpZ4hzfu+aQZmEVOs9/A==", 
   "Integrity": {
     "Type": "integrity", 
-    "Value": "zIEca9NOoMeN7zjGeQ3e0kzrtEs=", 
+    "Value": "WGsTQNJiSUtALrSqpnxJrZTT9v4=", 
     "Algorithm": "HMAC-SHA1"
   }
 };
@@ -84,14 +148,14 @@ var encrypted_pair={
   "Encryption": {
     "Type": "encryption", 
     "Algorithm": "AES-256-CBC", 
-    "IV": "0RNM7UQdaw1HUk483lN/jg=="
+    "IV": "TWHH3aYTqGRQVmb0bDHdzA=="
   }, 
   "Integrity": {
     "Type": "integrity", 
-    "Value": "w4s1ueQ90s5Z4yAD649Q7aHJukY=", 
+    "Value": "yAplPqdFOK2DjDPUiPlgB5Y057M=", 
     "Algorithm": "HMAC-SHA1"
   }, 
   "Version": "1.0", 
   "Type": "encrypted", 
-  "EncryptedData": "v0IeLPimS2weld22loZZsj/ykS99I8Xg9cWVGIiwmpCb3oHblh/+Zf/G7UcygB/F60GT7wyPHDhVd5yTAYmNEcT8JtoRUO1TuifqXcz9lggkhwftGlYZsz9MQNa1QH8n/mD9CmQuiDlCm2LHoSlA/p47TWgJ/nh6vnRUwIsEHuSWt6ZH7F0TdmefAK51Yr8sa+skSjHw2xJBBAfF/J1mKMhuyc+mnJXy4DWiVdNXBUCmjIISaey+6KCSeSlKjdhIeqZqt5LrA69gWglJQBh50k39CNtGkHc/PwuihsX6tDtRAD+kIQ72g+o2M4R4G1o3SJJcMKDMovF2Anh553+TolEdVhJAa5mkfdRPcvIOl3hpP7jK62M7/nQrCyMoOSRKswehpM6DO/OG41v8+DuSv9FSiDW12MDmKnNm70w5LHJRaZxHW7NZMFRAPR+5i8P1S2qAWu3aEaqA904Eel4H4fOpUtRpCK/0i6ldUcbH3wgc5dZORY9QfGxPbGaz90/IxE6nq3NpUYxadG9ijRs7RXmRX1ptfoXg4JO6+Bumz3SfkmI8ib8tDit4VymptYFACNlEMQ/QfMEneOmitO5JbG72RnahaeMn/sxd2DsHDMTZco+0eLAGOkitG7bXG5HNs8LSWfMszdVWyEDhbQ6v1QWeStB9L0uujdDxShYupEGYFJrAD9obiZ9fwROuvtuX"
+  "EncryptedData": "DNJJtsEDe4doxb5h6dkhE9zH9Uy4JeqfAaXXtUfm8odZ+iS0nMAEmyFJM8rNvTO5ptdYE3TaxVHUgvXaMOLoR0AuWnoQT4vK2+lTxsyWh5aQS4dPxg8hCWEq2kmfhuM/DHBJ3NMbEF4E4MxrTO8DDf0q6ASNHNftB2PzFDsavmnTmPB7o0ZSM+R1p7oh4L0cPWQkI5wU8i02/aYx7rPhM89W93BXms39iKX7oebowtykn6muoICqTRYuEeBk63PycRsUDdg5YXiVzPzddvIadmP0gObTgRlICXKPQUdOx0k4750tTe4QMyS2dUE6Vy/wL5oGDE80b7M4guJt1boiqmWWRfb3J7oM9Ay9GHQohuMqM3+wzUrTDW/IjudZLA7uNTY6Md1rrUz3FuFE0BrjyxAjGWKP5HgN3UKHDMmhxeeH03vVOv/5Xzq4z8iUzNsyD7eVWCBfcdcbkxCVViyE+H0YsQgGyXqCgcUeqXhM6P44mcOquP+02HGs/2UgcFn95SF+QcUZhhJUk4e25+C210dS8pxSWI6zAwmpLM5ifh5ciT8WP47WVTiAOvegZmulubq6dNaMnTlBP86TWrGPdTUhGFQuiicgAQZDFNS9yrjF6Y8jILxr9lsCsjchE4i9F9CcTO6VB7eCgexDw8cBx8wtBah8BpbhE3IFVF0lcqjY4DK1qDNNZvlsKOJqHoKD"
 };
