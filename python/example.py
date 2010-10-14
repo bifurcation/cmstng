3b1e3a2027f8ec28a7112c1a80c6f0cedef3f250
hildjj@gmail.com <hildjj@gmail.com@60afc23b-3570-b480-5588-d332f296fb10>
2010-10-14 15:57:29 +0000

    Make example work again.

--- python/example.py
+++ python/example.py
@@ -1,6 +1,4 @@
-import datetime
-import crypto
-from crypto import b64, b64d, get_date, version, JSONdumps
+from crypto import *
 
 
 # The interior message we want to protect
@@ -11,39 +9,35 @@ inner_message = {
     }
 
 def sign_message(msg, signer_certs, signer_priv, digest_algorithm="SHA1"):
-    sig = crypto.Signed(data=message_text)
+    sig = Signed(data=message_text)
     sig.sign(signer_priv, signer_certs, digest_algorithm)
     return sig
 
 def verify_message(msg):
-    return msg.verify()
+    return msg.verify(True)
 
 def encrypt_message(msg, recipient_cert, encryption_algorithm, integrity_algorithm):
-    e = crypto.Encrypted(msg)
+    e = Encrypted(msg)
     e.encrypt([recipient_cert,], encryption_algorithm, integrity_algorithm)
     return e
     
 def decrypt_message(msg, privKey, name):
-    d = msg.decrypt(privKey, name)
-    return d["Data"]
+    d = msg.decrypt(privKey, name=name)
+    return d.Data
 
 if __name__ == '__main__':
-    ekr_kp = crypto.KeyPair('ekr@rtfm.com')
-    ekr_cert = ekr_kp.Certificate
-    ekr_priv = ekr_kp.Privkey
-    #ekr_priv = crypto.PrivateKey(size=1024)
-    #ekr_cert =  ekr_priv.PublicKey.genCertificate('ekr@rtfm.com')
-    ekr_certs = [ ekr_cert, ]
+    ekr_kp = PrivateKey()
+    ekr_cert = Certificate('ekr@rtfm.com', ekr_kp.PublicKey, validityDays=7)
 
     # Sign a message
-    signed = sign_message(inner_message, ekr_certs, ekr_priv)
+    signed = sign_message(inner_message, ekr_cert, ekr_kp)
     print "***** Signed message ****"
     print JSONdumps(signed, indent=2)
     print "*****"
 
     # Verify a message
     result = verify_message(signed)
-    if result != True:
+    if not result:
         print "Verification failed"
     else:
         print "Verification succceeded"
@@ -52,5 +46,5 @@ if __name__ == '__main__':
     encrypted = encrypt_message(message_text, ekr_cert, "AES-256-CBC", "HMAC-SHA1")
     print JSONdumps(encrypted, indent=2)
 
-    decrypted = decrypt_message(encrypted, ekr_priv, ekr_cert.Name)
+    decrypted = decrypt_message(encrypted, ekr_kp, ekr_cert.Name)
     print decrypted
